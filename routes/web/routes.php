b67b58df70486850364753e30de7daaabefeb7be
Steve McDougall <juststevemcd@gmail.com>
2024-05-28 10:28:47 +0100

    Organising breeze auth routes

--- routes/web/routes.php
+++ routes/web/routes.php
@@ -10,30 +10,18 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register');
+Route::middleware(['guest'])->group(static function (): void {
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
+    Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+});
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
+Route::middleware(['auth','throttle:6,1'])->group(static function (): void {
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->name('verification.send');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::middleware(['signed'])->group(static function (): void {
+        Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->name('verification.verify');
+    });
+});
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.email');
-
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.store');
-
-Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-    ->middleware(['auth', 'signed', 'throttle:6,1'])
-    ->name('verification.verify');
-
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth', 'throttle:6,1'])
-    ->name('verification.send');
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
